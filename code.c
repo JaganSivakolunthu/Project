e2662745e5b61c955cac8f2044cd8df58e163203
Jagan S <wittyjag@gmail.com>
2025-09-21 18:48:36 +0530

    Update code.c

--- code.c
+++ code.c
@@ -19,6 +19,7 @@ typedef struct issue{
 	char due_dt[30];
 	struct issue *nxt;
 } ISS;
+void add(LD **);
 void add_book(LD **);
 void print(LD **);
 void add_iss(LD *, ISS **, struct tm *);
@@ -26,27 +27,51 @@ void iss_print(ISS **);
 void update_book_details(LD **);
 void up_b_id(LD **);
 void up_b_nm(LD **);
+void search_book(LD **);
+void srch_b_id(LD **);
+void srch_b_nm(LD **);
+void srch_b_ar(LD **);
+void search_menu_dp();
+void remove_menu_dp();
+void update_menu_dp();
+void choice_menu_dp();
 char* due_date(struct tm *);
 char* issue_date(struct tm *);
 int main()
 {
-    char opt;
-    LD *hptr = NULL;
-    ISS *hp = NULL;
+    int opt;
     struct tm *a;
     time_t present = time(NULL);
     a = localtime(&present);
+    LD *hptr = NULL;
+    ISS *hp = NULL;
+    while(1){
+    choice_menu_dp();
+    scanf("%d",&opt);
+    switch(opt)
+    {
+	    case 1:	add(&hptr);	break;
+	    case 2:	update_book_details(&hptr);	break;
+	    //case 3:	
+	    case 4:	search_book(&hptr);	break;
+	    case 5:	print(&hptr);	break;
+	    case 6:	add_iss(hptr,&hp,a);	break;
+	    //case 7:
+	    case 8:	iss_print(&hp);	break;
+	    //case 9:
+            //case 10:
+	    default: puts("Invalid Choice!!!");
+    }
+    }
+}
+void add(LD **hp)
+{
+    char opt;
     do{
-        add_book(&hptr);
+        add_book(hp);
         puts("Want to add one more data...(y/n)");
         scanf(" %c",&opt);
     }while(opt == 'y');
-    print(&hptr);
-    add_iss(hptr,&hp,a);
-    iss_print(&hp);
-    print(&hptr);
-    update_book_details(&hptr);
-    print(&hptr);
 }
 void add_book(LD **ptr)
 {
@@ -123,6 +148,7 @@ char* issue_date(struct tm *a)
 void update_book_details(LD **ptr)
 {
 	int opt;
+	update_menu_dp();
 	scanf("%d",&opt);
 	switch(opt)
 	{
@@ -162,3 +188,113 @@ void up_b_nm(LD **ptr)
 		p = p->nxt;
 	}
 }
+void search_book(LD **ptr)
+{
+	int opt;
+	search_menu_dp();
+	scanf("%d",&opt);
+	switch(opt)
+	{
+		case 1: srch_b_id(ptr);	break;
+		case 2:	srch_b_nm(ptr);	break;
+		case 3: srch_b_ar(ptr);	break;
+		//case 4: 
+		default: puts("Invalid Choice");
+	}
+}
+void srch_b_id(LD **ptr)
+{
+	LD *p = *ptr;
+        int id;
+	puts("Enter the ID which is to be searched...");	
+	scanf("%d",&id);
+	while(p)
+	{
+		if(id == (p->b_id))
+			printf("%d %s %s %d\n",p->b_id,p->b_nm,p->b_ar,p->b_cp);
+		p = p->nxt;
+	}
+}
+void srch_b_nm(LD **ptr)
+{
+	LD *p = *ptr;
+        char arr[50];
+	puts("Enter the book name which is to be searched...");	
+	scanf("%s",arr);
+	while(p)
+	{
+		if(strcmp(arr,p->b_nm)==0)
+			printf("%d %s %s %d\n",p->b_id,p->b_nm,p->b_ar,p->b_cp);
+		p = p->nxt;
+	}
+}
+void srch_b_ar(LD **ptr)
+{
+	LD *p = *ptr;
+        char arr[50];
+	puts("Enter the book name which is to be searched...");	
+	scanf("%s",arr);
+	while(p)
+	{
+		if(strcmp(arr,p->b_ar)==0)
+			printf("%d %s %s %d\n",p->b_id,p->b_nm,p->b_ar,p->b_cp);
+		p = p->nxt;
+	}
+}
+void choice_menu_dp(void)
+{
+    printf(" __________________________________\n");
+    printf("|                                  |\n");
+    printf("| ***** BOOK MANAGEMENT MENU ***** |\n");
+    printf("|__________________________________|\n");
+    printf("| 1. Add New Book                  |\n");
+    printf("| 2. Update Book Details           |\n");
+    printf("| 3. Remove Book                   |\n");
+    printf("| 4. Search Book                   |\n");
+    printf("| 5. View All Books                |\n");
+    printf("| 6. Issue Book                    |\n");
+    printf("| 7. Return Book                   |\n");
+    printf("| 8. List Issued Books             |\n");
+    printf("| 9. Save                          |\n");
+    printf("| 10. Exit                         |\n");
+    printf("|    Enter the choice option :     |\n");
+    printf("|__________________________________|\n");
+}
+void update_menu_dp(void)
+{
+    printf(" __________________________________\n");
+    printf("|                                  |\n");
+    printf("| ***** UPDATE BOOK DETAILS  ***** |\n");
+    printf("|__________________________________|\n");
+    printf("| 1. By Book_ID                    |\n");
+    printf("| 2. By Book Name                  |\n");
+    printf("| 3. Back To Main Menu             |\n");
+    printf("|    Enter the choice option :     |\n");
+    printf("|__________________________________|\n");
+}
+void remove_menu_dp(void)
+{
+    printf(" __________________________________\n");
+    printf("|                                  |\n");
+    printf("| ***** REMOVE BOOK DETAILS  ***** |\n");
+    printf("|__________________________________|\n");
+    printf("| 1. By Book_ID                    |\n");
+    printf("| 2. By Book Name                  |\n");
+    printf("| 3. Back To Main Menu             |\n");
+    printf("|    Enter the choice option :     |\n");
+    printf("|__________________________________|\n");
+}
+void search_menu_dp(void)
+{
+    printf(" __________________________________\n");
+    printf("|                                  |\n");
+    printf("| ***** SEARCH BOOK DETAILS  ***** |\n");
+    printf("|__________________________________|\n");
+    printf("| 1. By Book_ID                    |\n");
+    printf("| 2. By Book Name                  |\n");
+    printf("| 3. By Author Name                |\n");
+    printf("| 4. Back To Main Menu             |\n");
+    printf("|    Enter the choice option :     |\n");
+    printf("|__________________________________|\n");
+}
+
